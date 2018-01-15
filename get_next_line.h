aa15eb93bc94ea4390bc518318cd1c57ee6d61dd
Lisa TOULZAC <litoulza@e1r7p16.42.fr>
2018-01-15 17:20:36 +0100

    all

--- get_next_line.h
+++ get_next_line.h
@@ -1,7 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: litoulza <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/15 15:18:34 by litoulza          #+#    #+#             */
+/*   Updated: 2018/01/15 16:05:32 by litoulza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 6
-# include "libft.h"
+# include "libft/libft.h"
 # include <stdlib.h>
 # include <sys/types.h>
 # include <sys/uio.h>
